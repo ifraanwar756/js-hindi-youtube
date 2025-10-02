6947577a98e9202cf7ca6493993b323614d8e7f2
ifraanwar756 <ifraanwar756@gmail.com>
2025-10-02 09:19:32 +0000

    committed done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "ifraanwar123@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+
+//accountId = 2  //not allowed
+
+console.log(accountId);
+accountEmail = "hc@hc.com"
+accountPassword = "4535454"
+accountCity = "Bengalure"
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
