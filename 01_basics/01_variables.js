ff53deb7a64e1d40f7cc554d3c8c7f2a6f3e12b2
ifraanwar756 <ifraanwar756@gmail.com>
2025-10-02 15:18:43 +0000

    commit variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,13 +2,19 @@ const accountId = 144553
 let accountEmail = "ifraanwar123@gmail.com"
 var accountPassword = "12345"
 accountCity = "Jaipur"
+let accountState;
 
 
 //accountId = 2  //not allowed
+
+/*
+prefer not to use var...
+because of issue in block scope and functional scope
+*/
 
 console.log(accountId);
 accountEmail = "hc@hc.com"
 accountPassword = "4535454"
 accountCity = "Bengalure"
 
-console.table([accountId, accountEmail, accountPassword, accountCity]);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
