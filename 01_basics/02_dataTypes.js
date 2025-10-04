411e3961a8200cbcc2a8ee74ae4c3f8dd8584f8f
ifraanwar756 <ifraanwar756@gmail.com>
2025-10-04 06:59:22 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+let name = "Ifra"
+let age = 18
+
+
+// alert("hello");  //we are using node (alert works in browser to print)
+
+/*data types discussion
+
+number => 2 to power 34
+bigint
+string => ""
+boolean => true/false
+null => standalone value(when we want a empty value from a server)
+undefined => when we dont assign a value to a datatype
+symbol => find uniquenes of components
+
+object => 
+
+*/
+
+console.log(typeof age);         //to know about the datatype 
+console.log(typeof "Ifra");      //like it is a string datatype
+console.log(typeof undefined);   //undefined datatype
+console.log(typeof null);        //object datatype
