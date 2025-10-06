99a74c04f7a78bf7fa103b9f44e21a2da5f8d5ec
ifraanwar756 <ifraanwar756@gmail.com>
2025-10-06 10:36:30 +0000

    dataTypes-summary

--- 01_basics/dataTypes-summary.js
+++ 01_basics/dataTypes-summary.js
@@ -6,22 +6,22 @@
 //Array, Objects, Functions
 
 //STATICALLY TYPED LANGUAGE
-//When you declare data type at the before compiling
+//When you declare data type before compiling
 //examples --> Java, C, C++
 
 //DYNAMICALLY TYPED LANGUAGE
 //You don’t need to declare variable types explicitly.
 //examples --> JavaScript, Python
 
-//..................PRIMITIVE DATA TYPE...............
+//.................. PRIMITIVE DATA TYPE ...............
 
-const Id = Symbol("123")               //use of symbol data type
+/*const Id = Symbol("123")               //use of symbol data type
 const anotherId = Symbol("123");
 console.log(Id === anotherId);
 
 const BigInt = 423452344521515151n;    //use of bigInt
 
-//.................REFERENCE DATA TYPE...........
+//................. REFERENCE DATA TYPE ...........
 
 const heroes = ["Salman Khan", "Shahrukh Khan", "Ranbeer Kapoor"];   //use of array datatype
 
@@ -34,5 +34,53 @@ let myObj = {                 //use of object data type
 const myFunction = function(){    //use of function datatype
     console.log("Hello World");
 
+}*/
+
+//*************************** STACK AND HEAP MEMORY **************************//
+
+//Stack(Primitive).....Heap(Non Primitive)
+
+
+//**************** EXAMPLE OF STACK***********************
+let myYouTubeName = "ifraanwar" 
+
+let anotherName = myYouTubeName
+anotherName = "ifra"                 //in stack changes occur in copied value
+
+console.log(myYouTubeName);
+console.log(anotherName);
+
+
+//****************** MORE EXAMPLE ************************
+let a = 10;   // stored directly in stack
+let b = a;    // copy of value is made
+b = 20;
+
+console.log(a);      // 10
+console.log(b);     // 20 changing the value of b doesnt affect the value of a 
+
+
+//****************** EXAMPLE OF HEAP**********************
+
+let user1 = {                        
+    email: "user1@google.com",
+    upi: "ian@ybl"
 }
+
+let user2 = user1;                 //in heap changes occur in original value
+
+user2.email = "user2@google.com"   //if changes occur in user2 user1 will automatically changed
+
+console.log(user1.upi);
+console.log(user2.email);
+
+//************************ MORE EXAMPLE ***********************
+
+let obj1 = { name: "Ifra" };
+let obj2 = obj1;   // obj2 points to the same heap object
+
+obj2.name = "Anwar";
+
+console.log(obj1.name); // "Anwar" ✅ both refer to same object
+
 
