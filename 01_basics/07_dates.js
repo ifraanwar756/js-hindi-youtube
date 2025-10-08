bc1b1e6e57a265015455712011cd8aa728a158ce
ifraanwar756 <ifraanwar756@gmail.com>
2025-10-08 15:26:56 +0000

    dates

--- 01_basics/07_dates.js
+++ 01_basics/07_dates.js
@@ -12,7 +12,7 @@ let myCreatedDateInDiffFormat = new Date("2023-01-14");   //show any date
 
 console.log(myCreatedDate.toLocaleString());
 console.log(myCreatedDateWithTime.toLocaleString());
-console.log(myCreatedDateInDiffFormat.toLocaleString();
+console.log(myCreatedDateInDiffFormat.toLocaleString());
 
 let now = Date.now();   // milliseconds since 1970 upto now
 console.log("Current timestamp:", now);
